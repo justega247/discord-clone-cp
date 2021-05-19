eb5c658f04ff6c0b94cab4460f149ae39741509d
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2021-05-19 16:21:06 +0100

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -6,14 +6,17 @@ import "./Login.css";
 function Login() {
   const signIn = () => {
     auth.signInWithPopup(provider).catch((err) => alert(err.message));
-  }
+  };
 
   return (
     <div className="login">
       <div className="login__logo">
-        <img src="https://upload.wikimedia.org/wikipedia/sco/thumb/9/98/Discord_logo.svg/800px-Discord_logo.svg.png" alt=""/>
+        <img
+          src="https://logos-download.com/wp-content/uploads/2021/01/Discord_Logo_full.png"
+          alt=""
+        />
       </div>
-      <Button onClick={signIn}>Sign In</Button>
+      <Button onClick={signIn}>Sign In here</Button>
     </div>
   );
 }
